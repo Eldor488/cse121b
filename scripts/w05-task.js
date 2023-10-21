c1741b3f13e1648b27e0af13844120ca6cfabb2a
Eldor488 <del20047@byui.edu>
2023-10-21 01:15:41 -0600

    its done

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,41 +1,91 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
-const templesElement = document.querySelector('#temples');
 
-const templesList = [];
+
+let templesList = [];
 
 /* async displayTemples Function */
-const displayTemples = (temples) => {templesList.forEach(temple => {
-    const article = document.createElement('article');
-    const templeName = document.createElement('h3');
-    templeName.innerHTML = temple.templeName;
-    const templeImage = document.createElement('img');
-    templeImage.src = temple.imageUrl;
-    templeImage.alt = temple.location;
-    // document.article.appendChild(templeName);
-    // document.article.appendChild(templeImage);
-    // document.templesElement.appendChild(article);
-    // These corrections are necessary because 
-    // appendChild is a method that should be called on the parent element to 
-    // which you want to append a child element. 
-    // In your original code, document. would cause errors because document does not have properties named article or templesElement. Instead, you 
-    // should use the variables article and templesElement that you have already defined.
+function  displayTemples (temples)  {
+    let templesElement = document.querySelector('#temples');
+    temples.forEach(temple => {
+        const article = document.createElement('article');
+        const templeName = document.createElement('h3');
+        templeName.innerHTML = temple.templeName;
+        const templeImage = document.createElement('img');
+        templeImage.src = temple.imageUrl;
+        templeImage.alt = temple.location;
+        article.appendChild(templeName);
+        article.appendChild(templeImage);
+        templesElement.appendChild(article);
+    });
+   
+}
 
-});}
 
 
 
 /* async getTemples Function using fetch()*/
+getTemples = async () => {
+    const response = await fetch('https://byui-cse.github.io/cse121b-ww-course/resources/temples.json')
+    const data = await response.json();
+    templesList = data;
+    displayTemples(templesList);
 
+}
 
 /* reset Function */
+function reset () {
+    console.log("RESET");
+    document.getElementById('temples').innerHTML = "";
+}
+
+// Call the reset function
+
+
+
+
+// Should log the actual HTML element, not null or undefined
 
 
 /* sortBy Function */
 
+function sortBy(temples) {
+    reset();
+    const compareDate = new Date(1950, 0, 1);
+    let filter = document.querySelector('#sortBy').value;
+    console.log("sortBy function called with filter:", filter);
+    let currTemples;
+    switch (filter) {
+        case 'utah':
 
-
-getTemples();
+            currTemples = temples.filter(function(temple) {return temple.location.includes('Utah')})
+            displayTemples(currTemples);
+            console.log(currTemples);
+            break;
+        case 'notutah':
+            currTemples = temples.filter(function(temple) {return !temple.location.includes('Utah')})
+            displayTemples(currTemples);
+            console.log(currTemples);
+            break;
+        case 'older':
+            displayTemples(temples.filter(function(temple)  {
+                const templeDate = new Date(temple.dedicated.split(', ').reverse().join('-'));
+                console.log(temples);
+                return templeDate < compareDate;}));
+            break;
+        case 'all':
+            displayTemples(temples);
+            break;
+        default:
+            displayTemples(temples);
+    }
+};
 
 /* Event Listener */
+
+document.querySelector('#sortBy').addEventListener('change', function() {
+    sortBy(templesList)
+});
+
+getTemples();
