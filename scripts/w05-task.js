4cf53da58d1046589871972eee9e29e15d6a6283
Eldor488 <del20047@byui.edu>
2023-10-20 21:11:17 -0600

    whatever

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -13,9 +13,15 @@ const displayTemples = (temples) => {templesList.forEach(temple => {
     const templeImage = document.createElement('img');
     templeImage.src = temple.imageUrl;
     templeImage.alt = temple.location;
-    document.article.appendChild(templeName);
-    document.article.appendChild(templeImage);
-    document.templesElement.appendChild(article);
+    // document.article.appendChild(templeName);
+    // document.article.appendChild(templeImage);
+    // document.templesElement.appendChild(article);
+    // These corrections are necessary because 
+    // appendChild is a method that should be called on the parent element to 
+    // which you want to append a child element. 
+    // In your original code, document. would cause errors because document does not have properties named article or templesElement. Instead, you 
+    // should use the variables article and templesElement that you have already defined.
+
 });}
 
 
