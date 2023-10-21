439fd890bab0b335bf9ac72af29ebd0cb8a8298e
Eldor488 <del20047@byui.edu>
2023-10-20 21:07:35 -0600

    document. is not necessary here cause

--- scripts/w05-task.js
+++ scripts/w05-task.js
@@ -1,10 +1,22 @@
 /* W05: Programming Tasks */
 
 /* Declare and initialize global variables */
+const templesElement = document.querySelector('#temples');
 
+const templesList = [];
 
 /* async displayTemples Function */
-
+const displayTemples = (temples) => {templesList.forEach(temple => {
+    const article = document.createElement('article');
+    const templeName = document.createElement('h3');
+    templeName.innerHTML = temple.templeName;
+    const templeImage = document.createElement('img');
+    templeImage.src = temple.imageUrl;
+    templeImage.alt = temple.location;
+    document.article.appendChild(templeName);
+    document.article.appendChild(templeImage);
+    document.templesElement.appendChild(article);
+});}
 
 
 
